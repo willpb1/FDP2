d847664c4fa77de0104d7f15e4b9ca237f23546e
willpb1 <134088348+willpb1@users.noreply.github.com>
2023-09-29 10:18:18 -0400

    Add files via upload

---
+++ calcular raiz/raiz.cs
@@ -0,0 +1,237 @@
+﻿namespace raiz
+{
+    partial class Form1 : Form1Base
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            btnCuatro = new Button();
+            btnTres = new Button();
+            btnSeis = new Button();
+            btnNueve = new Button();
+            btnOcho = new Button();
+            btnCinco = new Button();
+            btnDos = new Button();
+            btnCero = new Button();
+            btnSiete = new Button();
+            btnUno = new Button();
+            btnSigno = new Button();
+            btnRaiz = new Button();
+            btnPunto = new Button();
+            btnBorrarTodo = new Button();
+            btnBorrar = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "txtBox1";
+            textBox1.Size = new Size(256, 33);
+            textBox1.TabIndex = 0;
+            // 
+            // btnCuatro
+            // 
+            btnCuatro.Location = new Point(12, 118);
+            btnCuatro.Name = "btnCuatro";
+            btnCuatro.Size = new Size(60, 60);
+            btnCuatro.TabIndex = 1;
+            btnCuatro.Text = "4";
+            btnCuatro.UseVisualStyleBackColor = true;
+            // 
+            // btnTres
+            // 
+            btnTres.Location = new Point(144, 184);
+            btnTres.Name = "btnTres";
+            btnTres.Size = new Size(60, 60);
+            btnTres.TabIndex = 2;
+            btnTres.Text = "3";
+            btnTres.UseVisualStyleBackColor = true;
+            // 
+            // btnSeis
+            // 
+            btnSeis.Location = new Point(144, 117);
+            btnSeis.Name = "btnSeis";
+            btnSeis.Size = new Size(60, 60);
+            btnSeis.TabIndex = 3;
+            btnSeis.Text = "6";
+            btnSeis.UseVisualStyleBackColor = true;
+            // 
+            // btnNueve
+            // 
+            btnNueve.Location = new Point(144, 51);
+            btnNueve.Name = "btnNueve";
+            btnNueve.Size = new Size(60, 60);
+            btnNueve.TabIndex = 4;
+            btnNueve.Text = "9";
+            btnNueve.UseVisualStyleBackColor = true;
+            // 
+            // btnOcho
+            // 
+            btnOcho.Location = new Point(78, 52);
+            btnOcho.Name = "btnOcho";
+            btnOcho.Size = new Size(60, 60);
+            btnOcho.TabIndex = 5;
+            btnOcho.Text = "8";
+            btnOcho.UseVisualStyleBackColor = true;
+            // 
+            // btnCinco
+            // 
+            btnCinco.Location = new Point(78, 118);
+            btnCinco.Name = "btnCinco";
+            btnCinco.Size = new Size(60, 60);
+            btnCinco.TabIndex = 6;
+            btnCinco.Text = "5";
+            btnCinco.UseVisualStyleBackColor = true;
+            // 
+            // btnDos
+            // 
+            btnDos.Location = new Point(78, 184);
+            btnDos.Name = "btnDos";
+            btnDos.Size = new Size(60, 60);
+            btnDos.TabIndex = 7;
+            btnDos.Text = "2";
+            btnDos.UseVisualStyleBackColor = true;
+            // 
+            // btnCero
+            // 
+            btnCero.Location = new Point(78, 250);
+            btnCero.Name = "btnCero";
+            btnCero.Size = new Size(60, 60);
+            btnCero.TabIndex = 8;
+            btnCero.Text = "0";
+            btnCero.UseVisualStyleBackColor = true;
+            // 
+            // btnSiete
+            // 
+            btnSiete.Location = new Point(12, 51);
+            btnSiete.Name = "btnSiete";
+            btnSiete.Size = new Size(60, 60);
+            btnSiete.TabIndex = 9;
+            btnSiete.Text = "7";
+            btnSiete.UseVisualStyleBackColor = true;
+            // 
+            // btnUno
+            // 
+            btnUno.Location = new Point(12, 184);
+            btnUno.Name = "btnUno";
+            btnUno.Size = new Size(60, 60);
+            btnUno.TabIndex = 10;
+            btnUno.Text = "1";
+            btnUno.UseVisualStyleBackColor = true;
+            // 
+            // btnSigno
+            // 
+            btnSigno.Location = new Point(12, 250);
+            btnSigno.Name = "btnSigno";
+            btnSigno.Size = new Size(60, 60);
+            btnSigno.TabIndex = 11;
+            btnSigno.Text = "+/-";
+            btnSigno.UseVisualStyleBackColor = true;
+            // 
+            // btnRaiz
+            // 
+            btnRaiz.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRaiz.Location = new Point(210, 183);
+            btnRaiz.Name = "btnRaiz";
+            btnRaiz.Size = new Size(60, 127);
+            btnRaiz.TabIndex = 12;
+            btnRaiz.Text = "√";
+            btnRaiz.UseVisualStyleBackColor = true;
+            // 
+            // btnPunto
+            // 
+            btnPunto.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPunto.Location = new Point(144, 250);
+            btnPunto.Name = "btnPunto";
+            btnPunto.Size = new Size(60, 60);
+            btnPunto.TabIndex = 13;
+            btnPunto.Text = ".";
+            btnPunto.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrarTodo
+            // 
+            btnBorrarTodo.Location = new Point(210, 117);
+            btnBorrarTodo.Name = "btnBorrarTodo";
+            btnBorrarTodo.Size = new Size(60, 60);
+            btnBorrarTodo.TabIndex = 14;
+            btnBorrarTodo.Text = "C";
+            btnBorrarTodo.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(210, 52);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(60, 60);
+            btnBorrar.TabIndex = 15;
+            btnBorrar.Text = "CE";
+            btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(280, 322);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnBorrarTodo);
+            Controls.Add(btnPunto);
+            Controls.Add(btnRaiz);
+            Controls.Add(btnSigno);
+            Controls.Add(btnUno);
+            Controls.Add(btnSiete);
+            Controls.Add(btnCero);
+            Controls.Add(btnDos);
+            Controls.Add(btnCinco);
+            Controls.Add(btnOcho);
+            Controls.Add(btnNueve);
+            Controls.Add(btnSeis);
+            Controls.Add(btnTres);
+            Controls.Add(btnCuatro);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void PerformLayout()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SuspendLayout()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button btnCuatro;
+        private Button btnTres;
+        private Button btnSeis;
+        private Button btnNueve;
+        private Button btnOcho;
+        private Button btnCinco;
+        private Button btnDos;
+        private Button btnCero;
+        private Button btnSiete;
+        private Button btnUno;
+        private Button btnSigno;
+        private Button btnRaiz;
+        private Button btnPunto;
+        private Button btnBorrarTodo;
+        private Button btnBorrar;
+    }
+}
